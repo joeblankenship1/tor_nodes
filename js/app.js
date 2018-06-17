d101b632bcf228b13f2ec5edea79eecc6170ce77
joeblankenship1 <joe.blankenship1@gmail.com>
2018-06-16 20:52:18 -0400

    added event listeners to map and list click event - list events still need work

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
 (function() {
+    // This will let you use the .remove() function later on
+    if (!('remove' in Element.prototype)) {
+        Element.prototype.remove = function() {
+            if (this.parentNode) {
+                this.parentNode.removeChild(this);
+            }
+        };
+    }
     // add token
     mapboxgl.accessToken = 'pk.eyJ1Ijoiam9lYmxhbmtlbnNoaXAxIiwiYSI6ImNqZGRjY2F3ZjAwNTMyd3FoOG83c3A4aWsifQ.bXiHMzyYHgFukGwVPUddAw';
     // This adds the map to your page
@@ -18,6 +26,32 @@
             //console.log(geojson);
             addLayer(geojson);
             nodeLocationList(geojson);
+
+            map.on('click', function(e) {
+                var features = map.queryRenderedFeatures(e.point, {
+                    layers: ['locations']
+                });
+                if (features.length) {
+                    var clickedPoint = features[0];
+                    // 1. Fly to the point
+                    flyToStore(clickedPoint);
+                    // 2. Close all other popups and display popup for clicked store
+                    createPopUp(clickedPoint);
+                    // 3. Highlight listing in sidebar (and remove highlight for all other listings)
+                    var activeItem = document.getElementsByClassName('active');
+                    if (activeItem[0]) {
+                        activeItem[0].classList.remove('active');
+                    }
+                    var selectedFeature = clickedPoint.properties.Name;
+                    for (var i = 0; i < geojson.features.length; i++) {
+                        if (geojson.features[i].properties.Name === selectedFeature) {
+                            selectedFeatureIndex = i;
+                        }
+                    }
+                    var node = document.getElementById('node-' + selectedFeatureIndex);
+                    node.classList.add('active');
+                }
+            });
         });
     });
 
@@ -42,7 +76,7 @@
         // Iterate through the list of stores
         for (i = 0; i < geojson.features.length; i++) {
             var currentFeature = geojson.features[i];
-            // Shorten data.feature.properties to just `prop` so we're not
+            // Shorten geojson.feature.properties to just `prop` so we're not
             // writing this long form over and over again.
             var prop = currentFeature.properties;
             // Select the node container in the HTML and append a div
@@ -64,9 +98,42 @@
             // and fill it with the city and phone number
             var details = node.appendChild(document.createElement('div'));
             details.innerHTML = prop.description;
-            /*if (prop.phone) {
-                details.innerHTML += ' &middot; ' + prop.phoneFormatted;
-            }*/
         }
+
+        // Add an event listener for the links in the sidebar listing
+        link.addEventListener('click', function(e) {
+            // Update the currentFeature to the store associated with the clicked link
+            var clickedNode = geojson.features[this.dataPosition];
+            console.log(clickedNode);
+            // 1. Fly to the point associated with the clicked link
+            flyToStore(clickedNode);
+            // 2. Close all other popups and display popup for clicked store
+            createPopUp(clickedNode);
+            // 3. Highlight listing in sidebar (and remove highlight for all other listings)
+            var activeItem = document.getElementsByClassName('active');
+            if (activeItem[0]) {
+                activeItem[0].classList.remove('active');
+            }
+            this.parentNode.classList.add('active');
+        });
+    }
+
+    function flyToStore(currentFeature) {
+        map.flyTo({
+            center: currentFeature.geometry.coordinates,
+            zoom: 15
+        });
+    }
+
+    function createPopUp(currentFeature) {
+        var popUps = document.getElementsByClassName('mapboxgl-popup');
+        // Check if there is already a popup on the map and if so, remove it
+        if (popUps[0]) popUps[0].remove();
+
+        var popup = new mapboxgl.Popup({ closeOnClick: false })
+            .setLngLat(currentFeature.geometry.coordinates)
+            .setHTML('<h3>Tor Node - Exit Fast</h3>' +
+                '<h4>' + currentFeature.properties.Name + '</h4>')
+            .addTo(map);
     }
 })();
